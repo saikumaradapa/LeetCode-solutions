4cc9199979edeef1669bfbff276ce88a8903e239
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2025-10-30 22:48:01 +0530

    Refactor is_same function for symmetric tree check
    Simplify the condition for checking node values in is_same function.

--- 101. Symmetric Tree.py
+++ 101. Symmetric Tree.py
@@ -4,8 +4,12 @@ class Solution:
         def is_same(left, right) :
             if not left or not right :
                 return left==right
-            if left.val != right.val :
-                return False 
-            return is_same(left.left, right.right) and is_same(left.right, right.left)
+
+            return left.val == right.val and is_same(left.left, right.right) and is_same(left.right, right.left)
 
         return is_same(left,right)
+
+'''
+    time complexity : O(n)
+    space complexity : O(h)
+'''
