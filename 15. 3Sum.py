c8709328447979ba53e9e1889538261d1e3e224d
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2025-11-30 16:51:42 +0530

    Refactor threeSum to use set for unique results
    Refactor threeSum function to use a set for unique triplets and improve time complexity.

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -3,7 +3,7 @@ class Solution:
         n = len(nums)
         res = []
         nums.sort()
-        print(nums)
+
         for i in range(len(nums)-1) :
             if i > 0 and nums[i] == nums[i-1]: 
                 continue
@@ -27,31 +27,33 @@ class Solution:
 
 
 
-''' time complexity O(N^2) and space complexity O(N) for result '''
+''' 
+    time complexity O(n ^ 2) 
+    space complexity O(1)  
+'''
 
 
 
-
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+########################################################################################################################################################
 
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        N, result = len(nums), []
-        for i in range(N):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            target = nums[i]*-1
-            s,e = i+1, N-1
-            while s<e:
-                if nums[s]+nums[e] == target:
-                    result.append([nums[i], nums[s], nums[e]])
-                    s = s+1
-                    while s<e and nums[s] == nums[s-1]:
-                        s = s+1
-                elif nums[s] + nums[e] < target:
-                    s = s+1
-                else:
-                    e = e-1
-        return result
+        n = len(nums)
+        res = set()
+        
+        for i in range(n):
+            seen = set()
+            for j in range(i + 1, n):
+                target = -(nums[i] + nums[j])
+                if target in seen:
+                    res.add(tuple(sorted([nums[i], nums[j], target])))
+                seen.add(nums[j])
+                
+        return list(res)
+
+''' 
+    time complexity O(n ^ 2) 
+    space complexity O(n)  
+'''
+
