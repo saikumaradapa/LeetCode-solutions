c3fc256d1e92b28358a065a7ca3486cd73ae7244
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2026-01-01 23:04:58 +0530

    Update 15. 3Sum.py

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,28 +1,36 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         n = len(nums)
         res = []
-        nums.sort()
 
-        for i in range(len(nums)-1) :
-            if i > 0 and nums[i] == nums[i-1]: 
+        for i in range(n - 2): 
+            if nums[i] > 0: # if no -ve's we cann't get sum as 0
+                break
+
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            left, right = i+1, n-1
-            target = -nums[i]
-            while left < right :
-                if nums[left]+nums[right] > target:
-                    right -= 1
-                elif nums[left]+nums[right] < target:
+
+            left, right = i + 1, n - 1
+
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
+
+                if s < 0:
                     left += 1
-                else :
+                elif s > 0:
+                    right -= 1
+                else:
                     res.append([nums[i], nums[left], nums[right]])
-                    
-                    while left < right and nums[left] == nums[left+1] :
+
+                    while left < right and nums[left] == nums[left + 1]:
                         left += 1
-                    while left < right and nums[right] == nums[right-1] :
+                    while left < right and nums[right] == nums[right - 1]:
                         right -= 1
+
                     left += 1
                     right -= 1
+
         return res
 
 
