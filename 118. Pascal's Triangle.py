a2418da8247586cbcd15e3cc7c816c8e6268a10b
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2026-01-01 19:47:10 +0530

    Update 118. Pascal's Triangle.py

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -1,28 +1,68 @@
-import math
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        result = []
-        for i in range(numRows) :
-            temp = []
-            for j in range(i+1) :
-                temp.append(math.comb(i,j))
-            result.append(temp)
-        return result
-        
-        
+        triangle = []
 
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+            triangle.append(row)
 
+        return triangle
+
+''' if entire triangle is asked, use this method
+    time complexity : O(n ^ 2) 
+    space complexity : O(n ^ 2) - for output 
+'''
+
+######################################################################################################################################################
 
 class Solution:
-    def generate(self, n: int) -> List[List[int]]:
-        result = []
-        for i in range(n) :
-            temp = []
-            for j in range(i+1) :
-                if j ==0 or j == i :
-                    temp.append(1)
-                else :
-                    temp.append(result[i-1][j]+result[i-1][j-1])
-            result.append(temp)
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
 
-        return result
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            curr_val = 1  # C(i, 0)
+
+            for j in range(1, i):
+                curr_val *= (i - j + 1) 
+                curr_val //= j
+                row[j] = curr_val
+
+            triangle.append(row)
+
+        return triangle
+
+
+''' if paricular row is asked, use this method
+    time complexity : O(n ^ 2)
+    space complexity : O(n) - we don't need output triangle if one row is asked
+'''
+
+######################################################################################################################################################
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = self.get_ncr(i, j)
+            triangle.append(row)
+
+        return triangle
+    
+    def get_ncr(self, n, r):
+        res = 1
+        for i in range(r):
+            res *= (n - i)
+            res //= (i + 1)
+        return res
+
+''' if paricular position is asked, use this method
+    time complexity : O(n ^ 2) * O(r)
+    space complexity : O(1) - we don't need output triangle if one position is asked
+'''
+
