52a97cbc1a5c84d983d0978569eacb1eb04f8f30
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2026-01-02 21:45:45 +0530

    Create 961. N-Repeated Element in Size 2N Array.py

---
+++ 961. N-Repeated Element in Size 2N Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return nums[i]
+            if i + 2 < len(nums) and nums[i] == nums[i + 2]:
+                return nums[i]
+            if i + 3 < len(nums) and nums[i] == nums[i + 3]:
+                return nums[i]
+
+''' The repeated number must appear at distance 1, 2, or 3. With n repeated elements among 2n, spacing them farther is impossible.
+    time complexity : O(n)
+    space complexity : O(1)
+'''
