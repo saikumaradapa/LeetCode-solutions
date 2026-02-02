b5bb6e41b90a339f4e28a3b06ea840458e745604
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2026-02-02 19:57:33 +0530

    Update 31. Next Permutation.py

--- 31. Next Permutation.py
+++ 31. Next Permutation.py
@@ -1,6 +1,7 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
 
+        # [2, 1, 5, 4, 3, 0, 0] - use this example 
         # 1.find a digit from back side where it's previous digit is less then current 
         # if not found any such then return the sorted array because next permutation is not possible 
         # 2.swap that small digit with it's next greater digit on it's right side 
