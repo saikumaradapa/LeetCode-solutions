5d0ba1f0cd01fa95221dacd0fd00c9bd1f60aa60
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2026-08-14 22:45:42 +0530

    Create 3090. Maximum Length Substring With Two Occurrences.py

---
+++ 3090. Maximum Length Substring With Two Occurrences.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        l = 0 
+        freq = defaultdict(int)
+        res = 0
+        for r in range(len(s)):
+            freq[s[r]] += 1
+            while freq[s[r]] > 2:
+                freq[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
+
+''' 
+    time complexity : O(n)
+    space complexity : O(n)
+    sliding window approach
+'''
